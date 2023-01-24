307538b507b42e8df57dd5f137cf2fba4827aa25
MaixAmigo <62819634+kapamelia@users.noreply.github.com>
2023-01-24 11:40:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: '卡帕·阿梅',
+  title: '暴走的卡帕·阿梅',
   author: 'Kap Amelia',
   email: 'i@craigary.net',
   link: 'https://nobelium-angryamelia.vercel.app',
@@ -25,14 +25,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-PW0GKPXJB4' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
